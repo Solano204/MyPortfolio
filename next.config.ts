8ba110d180f37103a374e72184af9e1469daa7f8
Carlos Josue Lopez Solano <140025175+Solano204@users.noreply.github.com>
2026-04-12 07:08:33 -0600

    backup: before config update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Disable ESLint during build
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
